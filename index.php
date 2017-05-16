9deef421e3bc3b013669215f6e531ceb43840c1d
anthones <antoniedimovski@gmail.com>
2017-05-16 18:31:50 +0200

    Update index.php

--- index.php
+++ index.php
@@ -24,7 +24,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Weather Scraper</title>
+    <title>Временска прогноза</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
     <link rel="stylesheet" type="text/css" href="style.css">
     <!--[if lt IE 9]>
